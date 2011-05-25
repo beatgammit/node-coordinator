ee72444e425c4befa4cd8d182422df75e1218948
Jameson Little <otto@von-bismarck.(none)>
2011-05-25 10:19:09 -0600

    Changed parameters for latlongToUtm

--- test/latlong-conv.js
+++ test/latlong-conv.js
@@ -1,48 +1,12 @@
 var latlong = require('../lib/latlong'),
-    coordNE = {
-        latitude: 30.5,
-        longitude: 40.2
-    },
-    coordNW = {
-        latitude: 30.5,
-        longitude: -40.2
-    },
-    coordSE = {
-        latitude: -30.5,
-        longitude: 40.2
-    },
-    coordSW = {
-        latitude: -30.5,
-        longitude: -40.2
-    };
+    checksums = require('./checksums');
 
-console.log();
-console.log("Original values:");
-console.log();
+checksums.forEach(function (item) {
+    console.log("Check values:");
+    console.log(item);
 
-console.log("NE:", coordNE);
-console.log("NW:", coordNW);
-console.log("SE:", coordSE);
-console.log("SW:", coordSW);
+    console.log("UTM:", latlong.toUtm(item.latitude.decimal, item.longitude.decimal));
+    console.log("MGRS:", latlong.toMgrs(item.latitude.decimal, item.longitude.decimal));
 
-console.log();
-console.log();
-
-console.log("UTM");
-console.log();
-
-console.log("NE:", latlong.toUtm(coordNE.latitude, coordNE.longitude));
-console.log("NW:", latlong.toUtm(coordNW.latitude, coordNW.longitude));
-console.log("SE:", latlong.toUtm(coordSE.latitude, coordSE.longitude));
-console.log("SW:", latlong.toUtm(coordSW.latitude, coordSW.longitude));
-
-console.log();
-console.log();
-
-console.log("MGRS");
-console.log();
-
-console.log("NE:", latlong.toMgrs(coordNE.latitude, coordNE.longitude));
-console.log("NW:", latlong.toMgrs(coordNW.latitude, coordNW.longitude));
-console.log("SE:", latlong.toMgrs(coordSE.latitude, coordSE.longitude));
-console.log("SW:", latlong.toMgrs(coordSW.latitude, coordSW.longitude));
+    console.log();
+});

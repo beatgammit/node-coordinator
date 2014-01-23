dade6d58c5025f5c15392e93fdd1465355f90633
T. Jameson Little <t.jameson.little@gmail.com>
2014-01-23 11:57:39 -0700

    Added in AMD shim

--- coordinator.js
+++ coordinator.js
@@ -1,5 +1,13 @@
-(function () {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['./lib/convert'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.returnExports = factory();
+  }
+}(this, function (convert) {
     "use strict";
 
-    module.exports = require('./lib/convert');
-}());
+    return convert;
+}));
